5638c414958baa532adf5bdcf42409a28d3270db
Arief Saifuddien <ariefsaifuddien01@gmail.com>
2022-06-23 01:05:31 +0200

    updates

---
+++ php/oop/basic/classes.php
@@ -0,0 +1,71 @@
+<?php
+
+// // 1. class & method
+
+// // class (penyimpan data object : property & method)
+// class product {
+
+// }
+
+// // object
+// $a = new product();
+// $b = new product();
+
+// var_dump( $a );
+
+//  2. property & method
+
+class product {
+
+  // property (variable di object) // didahului dengan visibility
+  public
+    $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0;
+
+  // method (function di object) // boleh dengan visibility
+  // public function sayHello() {
+  //   return "Hello World";
+  // }
+
+  public function getLabel() {
+    // $this untuk membuat global var
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+// // memanggil property
+// $produk1 = new product();
+// // menimpa value
+// $produk1->judul = "naruto";
+// var_dump( $produk1 );
+
+// $produk2 = new product();
+// // menimpa value
+// $produk2->judul = "dragon";
+// var_dump( $produk2 );
+
+$product3 = new product();
+$product3->judul = 'naruto';
+$product3->penulis = 'masashi kishimoto';
+$product3->penerbit = 'shonen jump';
+$product3->harga = 20000;
+// var_dump( $product3 );
+
+$product4 = new product();
+$product4->judul = 'uncharter';
+$product4->penulis = 'neil druckman';
+$product4->penerbit = 'sony computer';
+$product4->harga = 15000;
+// var_dump( $product4 );
+
+// echo "Komik : $product3->penulis, $product3->penerbit";
+// echo "<br>";
+
+// // memanggil method (function di oop)
+// echo $product3->sayHello();
+
+echo "Komik : " . $product3->getLabel();
+echo "<hr>";
+echo "Game : " . $product4->getLabel();
---
+++ php/oop/basic/inheritances.php
@@ -0,0 +1,97 @@
+<?php
+
+// // constructor
+// class product {
+
+//   public $judul, $penulis, $penerbit, $harga, $jmlHalaman, $jam, $tipe;
+
+//   // __construct(value) // nilai default dimasukkan ke params
+//   public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $jam = 0, $tipe ) {
+
+//     $this->judul = $judul;
+//     $this->penulis = $penulis;
+//     $this->penerbit = $penerbit;
+//     $this->harga = $harga;
+//     $this->jmlHalaman = $jmlHalaman;
+//     $this->jam = $jam;
+//     $this->tipe = $tipe;
+//   }
+
+//   public function getLabel() {
+//     return "$this->penulis, $this->penerbit";
+//   }
+
+//   public function infoProduct() {
+//     $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+//     if ( $this->tipe == 'komik' ) {
+//       $str .= " - {$this->jmlHalaman} halaman.";
+//     } else if ( $this->tipe == 'game' ) {
+//       $str .= " - {$this->jam} jam.";
+//     }
+
+//     return $str;
+//   }
+// }
+
+// //pemanggilan construct
+// $product1 = new product( 'naruto', 'masashi kishimoto', 'shonen jump', 20000, 100, 0, 'komik' );
+// $product2 = new product( 'uncharter', 'neil druckman', 'sony computer', 15000, 0, 5, 'game' );
+
+// echo $product1->infoProduct();
+// echo $product2->infoProduct();
+
+// inheritance(pewarisan)
+
+// constructor
+class product {
+
+  public $judul, $penulis, $penerbit, $harga, $jmlHalaman, $jam;
+
+  // __construct(value) // nilai default dimasukkan ke params
+  public function __construct(
+    $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0,
+    $jmlHalaman = 0,
+    $jam = 0 ) {
+
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->jam = $jam;
+  }
+
+  public function getLabel() {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function infoProduct() {
+    return "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+  }
+}
+
+// inheritance (extends)
+class komik extends product {
+  public function infoProduct() {
+    return "komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} halaman.";
+  }
+}
+
+// inheritance (extends)
+class game extends product {
+  public function infoProduct() {
+    return "komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jam} jam.";
+  }
+}
+
+//pemanggilan construct
+$product1 = new komik( 'naruto', 'masashi kishimoto', 'shonen jump', 20000, 100, 0 );
+$product2 = new game( 'uncharter', 'neil druckman', 'sony computer', 15000, 0, 5 );
+
+echo $product1->infoProduct();
+echo '<hr>';
+echo $product2->infoProduct();
